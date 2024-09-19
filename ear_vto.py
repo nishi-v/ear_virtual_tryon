1a05df5d840f4c2c6c958ea00b75f84aa29015e8
nishi-v <nishitav@futuresoftindia.com>
2024-09-19 17:13:20 +0530

    added bearer token

--- ear_vto.py
+++ ear_vto.py
@@ -139,7 +139,7 @@ else:
 
         # Determine the resize factor
         is_circular = (earring_width == earring_height)
-        resize_factor = 0.10 if is_circular else 0.10
+        resize_factor = 0.025 if is_circular else 0.15
         new_width = int(earring_width * resize_factor)
         new_height = int(earring_height * resize_factor)
         obj_resized = cv2.resize(obj_np, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
